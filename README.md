befa4475d52ad4f68559fb08d94af7450eb89f96
oho <olivier.hoachuck@gmail.com>
2013-08-10 00:42:32 +0200

    Allow now download of specific session numbers only (+minor convenient options)

--- README.md
+++ README.md
@@ -19,16 +19,39 @@ First version is not updated anymore because it does not comply with current rul
 You will be prompted for your Apple Developer password. And SD videos will be downloaded by default.
 
 ### Options
-You can try `wwdcVideoPDFGet-curlVersion.sh -h` for more options. This second script allow you to choose for SD vs HD videos format to download.
+You can try `wwdcVideoPDFGet-curlVersion.sh -h` for more options.
 
-For downloading HD videos:
+This second script allow you to choose for SD vs HD videos format to download. But also would let you choose for instance to get only some specific sessions in stead of all videos.
 
- - `wwdcVideoPDFGet-curlVersion.sh -f HD <Apple Developer account login>`
+See what's `wwdcVideoPDFGet-curlVersion.sh -h` option currently say:
+
+		Usage: 	wwdcVideoPDFGet-curlVersion.sh [options] <Apple dev login>
+		Options:
+			-y <year>: select year (ex: -y 2012). Default year is 2013
+				Possible values for year: 2012, 2013, all
+				Warning: year 2012 videos download is not yet available
+			-f <format>: select video format type (SD or HD). Default video format is SD
+			-s <comma separated session numbers>: select which sessions you want to download
+			-v : verbose mode
+			-o <output path>: path where to download content (default is /Users/${USER}/Desktop/WWDC-2013)
+			
+		Examples:
+			- Download all PDFs and SD videos for 2013:
+  				wwdcVideoPDFGet-curlVersion.sh john.doe@me.com
+			- Download all PDFs and HD videos for 2013:
+  				wwdcVideoPDFGet-curlVersion.sh -f HD john.doe@me.com
+			- Download only session 201, 400 and 401 with SD videos for 2013:
+  				wwdcVideoPDFGet-curlVersion.sh -s 201,400,401 john.doe@me.com
+			- Download only session 201 and 400 with HD video for 2013:
+  				wwdcVideoPDFGet-curlVersion.sh -s 201,400 -f HD john.doe@me.com
+			- Download all PDFs and HD videos for 2013 in /Users/oho/Documents/WWDC-SESSIONS using verbose mode:
+  				wwdcVideoPDFGet-curlVersion.sh -v -f HD -o /Users/oho/Documents/WWDC-SESSIONS john.doe@me.com
 		
 
 More information on http://blog.hoachuck.biz/blog/2013/06/15/script-to-download-wwdc-2013-videos/
 
 ### Requirements
 Work on MAC OS X.
-Should be easily adapted for any Linux system.
+
+Should be working on Linux system (as long as you change output directory): never tested!.
 
--- wwdcVideoPDFGet-curlVersion.sh
+++ wwdcVideoPDFGet-curlVersion.sh
@@ -5,29 +5,45 @@
 # License: Do What You Want with it. But notice that this script come with no garanty and will not be maintained.
 # usage: wwdcVideoGet-curlVersion.sh <Apple-dev-account-login>
 # TODO: 
+#	- make 2012 videos download Possible
+#	- dynamically set WWDC_DIRNAME according to download year
 #	- wrong password does not give proper error message!
-#	- key should be get dynamically - this already happened to be changed
+#	- display some statistics: total time of download (+ begin and end), total downloaded size of content
 
 VERSION="1.2"
 DEFAULT_FORMAT="SD"
 DEFAULT_YEAR="2013"
+SELECTIVE_SESSION_MODE=false
+VERBOSE=false
+TMP_DIR="/tmp/wwdc2013.tmp"
+WWDC_DIRNAME="/Users/${USER}/Desktop/WWDC-2013"
 
 doGet2013 () {
 
 	ituneslogin=$1
 	itunespassword=$2
 	FORMAT=$3
-
-	WWDC_DIRNAME="/Users/${USER}/Desktop/WWDC-2013"
-	TMP_DIR="/tmp/wwdc2013.tmp"
-	mkdir -p $TMP_DIR
-
-	# Dynamically get the key value as this can change (it did change for instance when Apple had to turn down their developer Portal for a week)
-	key=$(curl -L https://developer.apple.com/iphone | grep 'login?&appIdKey=' | sed -e 's/\(.*login?&appIdKey=\)\(.*\)\(&.*\)/\2/' | awk 'NR==1 {print $1}')
 	
+	if [ ${VERBOSE} == true ];
+	then
+	  echo "Sessions to be downloaded: ${SESSION_WANTED}"
+	  echo "Output directory: ${WWDC_DIRNAME}"
+	fi
+
+	mkdir -p $TMP_DIR
+	# Dynamically get the key value as this can change (it did change for instance when Apple had to turn down their developer Portal for a week)
+	if [ ${VERBOSE} == true ];
+	then
+		echo "Getting appIDKey..."
+	fi
+	key=$(curl -s -L https://developer.apple.com/iphone | grep 'login?&appIdKey=' | sed -e 's/\(.*login?&appIdKey=\)\(.*\)\(&.*\)/\2/' | awk 'NR==1 {print $1}')
+	if [ ${VERBOSE} == true ];
+	then
+		echo "appIDKey: ${key}"	
+	fi
 	cookies=(--cookies=on --keep-session-cookies)
 
-	action=$(curl 'https://daw.apple.com/cgi-bin/WebObjects/DSAuthWeb.woa/wa/login?appIdKey='"${key}" | grep '\ action=' | awk '{ print $4 }' | cut -f2 -d"=" | sed -e "s/^.*\"\(.*\)\".*$/\1/") 
+	action=$(curl -s 'https://daw.apple.com/cgi-bin/WebObjects/DSAuthWeb.woa/wa/login?appIdKey='"${key}" | grep '\ action=' | awk '{ print $4 }' | cut -f2 -d"=" | sed -e "s/^.*\"\(.*\)\".*$/\1/") 
 
 	curl -s --cookie-jar $TMP_DIR/cookies.txt "https://daw.apple.com${action}" -d theAccountName="${ituneslogin}" -d theAccountPW="${itunespassword}" > /dev/null 
 
@@ -57,8 +73,8 @@ doGet2013 () {
 	# do the rm *.download only if files exist
 	FILES_LIST="$(ls ${WWDC_DIRNAME}/PDFs/*.download 2>/dev/null)"
 	if [ -z "$FILES_LIST" ]; then
-		echo "I see this is the first time the script is being run! Cool :)"
-		echo "All downloads will go to your Desktop/WWDC-2013 folder!"
+		#echo "All downloads will go to your Desktop/WWDC-2013 folder!"
+		:
 	else
 		echo "Some download was aborted last time you ran this script."
 		rm ${WWDC_DIRNAME}/PDFs/*.download	
@@ -68,16 +84,34 @@ doGet2013 () {
 	i=0
 	cat ${TMP_DIR}/video.html | grep -o -E 'href="(http:\/\/devstreaming.apple.com\/videos\/wwdc\/2013/[0-9a-zA-Z]*\/[0-9]{1,5}\/[0-9]{1,5}\.pdf\?dl=1+)"' | cut -d'"' -f2 | while read line; do 
 		session_number=`echo $line | grep -o -E '/[0-9]+.pdf' | grep -o -E [0-9]+`
-		dest_path="${WWDC_DIRNAME}/PDFs/${session_number} - ${title_array[$i]}.pdf"
-		if [ -f "${dest_path}" ]
+		if [ ${SELECTIVE_SESSION_MODE} ];
 		then
-			echo "${dest_path} already downloaded (nothing to do!)"
-		else
-			echo "downloading PDF for session ${session_number}: $line" 
+			if `echo ${SESSION_WANTED} | grep "${session_number}" 1>/dev/null 2>&1`
+			then
+				dest_path="${WWDC_DIRNAME}/PDFs/${session_number} - ${title_array[$i]}.pdf"
+				if [ -f "${dest_path}" ]
+				then
+					echo "${dest_path} already downloaded (nothing to do!)"
+				else
+					echo "downloading PDF for session ${session_number}: $line" 
 
-			curl $line > "${dest_path}.download"
+					curl $line > "${dest_path}.download"
 		
-			mv "${dest_path}.download" "${dest_path}"
+					mv "${dest_path}.download" "${dest_path}"
+				fi
+			fi
+		else
+			dest_path="${WWDC_DIRNAME}/PDFs/${session_number} - ${title_array[$i]}.pdf"
+			if [ -f "${dest_path}" ]
+			then
+				echo "${dest_path} already downloaded (nothing to do!)"
+			else
+				echo "downloading PDF for session ${session_number}: $line" 
+
+				curl $line > "${dest_path}.download"
+	
+				mv "${dest_path}.download" "${dest_path}"
+			fi
 		fi
 		((i+=1))
 	done
@@ -90,8 +124,8 @@ doGet2013 () {
 	# do the rm *.download only if files exist
 	FILES_LIST="$(ls ${WWDC_DIRNAME}/${FORMAT}-VIDEOs/*.download 2>/dev/null)"
 	if [ -z "$FILES_LIST" ]; then
-		echo "I see this is the first time you go for the videos themselves! Cool :)"
-		echo "All downloads will go to your Desktop/WWDC-2013 folder!"
+		#echo "All downloads will go to your Desktop/WWDC-2013 folder!"
+		:
 	else
 		echo "Some download was aborted last time you ran this script."
 		rm ${WWDC_DIRNAME}/${FORMAT}-VIDEOs/*.download	
@@ -106,16 +140,34 @@ doGet2013 () {
 	then
 		cat ${TMP_DIR}/video.html | grep -o -E 'href="(http:\/\/devstreaming.apple.com\/videos\/wwdc\/2013/[0-9a-zA-Z]*\/[0-9]{1,5}\/[0-9]{1,5}-HD\.mov\?dl=1+)"' | cut -d'"' -f2 | while read line; do 
 			session_number=`echo $line | grep -o -E '/[0-9]+-HD.mov' | grep -o -E [0-9]+`
-			dest_path="${WWDC_DIRNAME}/${FORMAT}-VIDEOs/${session_number} - ${title_array[$i]}-${FORMAT}.mov"
-			if [ -f "${dest_path}" ]
+			if [ ${SELECTIVE_SESSION_MODE} ];
 			then
-				echo "${dest_path} already downloaded (nothing to do!)"
+				if `echo ${SESSION_WANTED} | grep "${session_number}" 1>/dev/null 2>&1`
+				then
+					dest_path="${WWDC_DIRNAME}/${FORMAT}-VIDEOs/${session_number} - ${title_array[$i]}-${FORMAT}.mov"
+					if [ -f "${dest_path}" ]
+					then
+						echo "${dest_path} already downloaded (nothing to do!)"
+					else
+						echo "downloading ${FORMAT} Video for session ${session_number}: $line" 
+
+						curl $line > "${dest_path}.download"
+
+						mv "${dest_path}.download" "${dest_path}"
+					fi
+				fi
 			else
-				echo "downloading ${FORMAT} Video for session ${session_number}: $line" 
+				dest_path="${WWDC_DIRNAME}/${FORMAT}-VIDEOs/${session_number} - ${title_array[$i]}-${FORMAT}.mov"
+				if [ -f "${dest_path}" ]
+				then
+					echo "${dest_path} already downloaded (nothing to do!)"
+				else
+					echo "downloading ${FORMAT} Video for session ${session_number}: $line" 
 
-				curl $line > "${dest_path}.download"
+					curl $line > "${dest_path}.download"
 
-				mv "${dest_path}.download" "${dest_path}"
+					mv "${dest_path}.download" "${dest_path}"
+				fi
 			fi
 			((i+=1))
 		done
@@ -123,16 +175,34 @@ doGet2013 () {
 	then
 		cat ${TMP_DIR}/video.html | grep -o -E 'href="(http:\/\/devstreaming.apple.com\/videos\/wwdc\/2013/[0-9a-zA-Z]*\/[0-9]{1,5}\/[0-9]{1,5}-SD\.mov\?dl=1+)"' | cut -d'"' -f2 | while read line; do 
 			session_number=`echo $line | grep -o -E '/[0-9]+-SD.mov' | grep -o -E [0-9]+`
-			dest_path="${WWDC_DIRNAME}/${FORMAT}-VIDEOs/${session_number} - ${title_array[$i]}-${FORMAT}.mov"
-			if [ -f "${dest_path}" ]
+			if [ ${SELECTIVE_SESSION_MODE} ];
 			then
-				echo "${dest_path} already downloaded (nothing to do!)"
+				if `echo ${SESSION_WANTED} | grep "${session_number}" 1>/dev/null 2>&1`
+				then
+					dest_path="${WWDC_DIRNAME}/${FORMAT}-VIDEOs/${session_number} - ${title_array[$i]}-${FORMAT}.mov"
+					if [ -f "${dest_path}" ]
+					then
+						echo "${dest_path} already downloaded (nothing to do!)"
+					else
+						echo "downloading ${FORMAT} Video for session ${session_number}: $line" 
+
+						curl $line > "${dest_path}.download"
+
+						mv "${dest_path}.download" "${dest_path}"
+					fi
+				fi
 			else
-				echo "downloading ${FORMAT} Video for session ${session_number}: $line" 
+				dest_path="${WWDC_DIRNAME}/${FORMAT}-VIDEOs/${session_number} - ${title_array[$i]}-${FORMAT}.mov"
+				if [ -f "${dest_path}" ]
+				then
+					echo "${dest_path} already downloaded (nothing to do!)"
+				else
+					echo "downloading ${FORMAT} Video for session ${session_number}: $line" 
 
-				curl $line > "${dest_path}.download"
+					curl $line > "${dest_path}.download"
 
-				mv "${dest_path}.download" "${dest_path}"
+					mv "${dest_path}.download" "${dest_path}"
+				fi
 			fi
 			((i+=1))
 		done
@@ -158,7 +228,7 @@ doGet2012 () {
 }
 
 ##########################################################################################
-#######                      		   MAINE 									##########
+#######                      		   MAIN 									##########
 ##########################################################################################
 
 if [ $# -eq "0" ]
@@ -173,48 +243,76 @@ ituneslogin=${@: -1}
 FORMAT=${DEFAULT_FORMAT}
 YEAR=${DEFAULT_YEAR}
 
-while getopts ":hy:f:" opt; do
+while getopts ":hy:f:s:vo:" opt; do
   case $opt in
     h)
-	  echo "WWDC Videos and PDFs downloader (version ${VERSION})" >&2
-      echo ""
-	  echo "Usage: 	`basename $0` [options] <Apple dev login>"
-	  echo "Options:"
-      echo "  -y <year>: select year (ex: -y 2012). Default year is 2013" >&2
-      echo "	Possible values for year: 2012, 2013, all" >&2
-      echo "	Warning: year 2012 videos download is not yet available" >&2
-      echo "  -f <format>: select video format type (SD or HD). Default video format is SD" >&2
-      echo ""  >&2
-      echo "Example:"  >&2
-      echo "  `basename $0` -y 2013 -f HD john.doe@me.com"  >&2
-      echo ""
-      exit 0;
-      ;;
+	  	echo "WWDC Videos and PDFs downloader (version ${VERSION})" >&2
+      	echo ""
+	  	echo "Usage: 	`basename $0` [options] <Apple dev login>"
+	  	echo "Options:"
+      	echo "	-y <year>: select year (ex: -y 2012). Default year is 2013" >&2
+      	echo "		Possible values for year: 2012, 2013, all" >&2
+      	echo "		Warning: year 2012 videos download is not yet available" >&2
+      	echo "	-f <format>: select video format type (SD or HD). Default video format is SD" >&2
+      	echo "	-s <comma separated session numbers>: select which sessions you want to download" >&2
+      	echo "	-v : verbose mode" >&2
+      	echo "	-o <output path>: path where to download content (default is /Users/${USER}/Desktop/WWDC-2013)" >&2
+      	echo ""  >&2
+      	echo "Examples:"  >&2
+      	echo "	- Download all PDFs and SD videos for 2013:"  >&2
+      	echo "  		`basename $0` john.doe@me.com"  >&2
+      	echo "	- Download all PDFs and HD videos for 2013:"  >&2
+      	echo "  		`basename $0` -f HD john.doe@me.com"  >&2
+      	echo "	- Download only session 201, 400 and 401 with SD videos for 2013:"  >&2
+      	echo "  		`basename $0` -s 201,400,401 john.doe@me.com"  >&2
+      	echo "	- Download only session 201 and 400 with HD video for 2013:"  >&2
+      	echo "  		`basename $0` -s 201,400 -f HD john.doe@me.com"  >&2
+      	echo "	- Download all PDFs and HD videos for 2013 in /Users/oho/Documents/WWDC-SESSIONS using verbose mode:"  >&2
+      	echo "  		`basename $0` -v -f HD -o /Users/oho/Documents/WWDC-SESSIONS john.doe@me.com"  >&2
+      	echo ""
+      	exit 0;
+      	;;
     y)
-      if [ $OPTARG = "2012" ] || [ $OPTARG = "2013" ] || [ $OPTARG = "all" ];
-      then
-	  	YEAR=$OPTARG
-	  else
-	  	echo "Unknown specified year. Using ${YEAR}!"
-	  fi
-      ;;
+      	if [ $OPTARG = "2012" ] || [ $OPTARG = "2013" ] || [ $OPTARG = "all" ];
+      	then
+	  		YEAR=$OPTARG
+	  	else
+	  		echo "Unknown specified year. Using ${YEAR}!"
+	  	fi
+      	;;
     f)
-      if [ $OPTARG = "SD" ] || [ $OPTARG = "HD" ];
-      then
-	      FORMAT=$OPTARG
-	  else
-	  	echo "Unknown specified format. Using ${FORMAT} video format!"
-	  fi
-      ;;
+      	if [ $OPTARG = "SD" ] || [ $OPTARG = "HD" ];
+      	then
+	      	FORMAT=$OPTARG
+	  	else
+	  		echo "Unknown specified format. Using ${FORMAT} video format!"
+	  	fi
+      	;;
+    s)
+      	if [ $OPTARG > 0 ];
+      	then
+	    	SESSION_WANTED=$OPTARG
+	    	SELECTIVE_SESSION_MODE=true
+	  	else
+	  		echo "Session number does not look good!"
+	  	fi
+      	;;
+    v)
+      	echo "Verbose mode on"
+      	VERBOSE=true
+      	;;
+    o)
+	  	WWDC_DIRNAME=${OPTARG}
+	  	;;
     \?)
-      echo "Invalid option: -$OPTARG" >&2
-      echo "For help, please use: $0 -h"
-      exit 1
-      ;;
+      	echo "Invalid option: -$OPTARG" >&2
+      	echo "For help, please use: $0 -h"
+      	exit 1
+      	;;
     :)
-      echo "Option -$OPTARG requires an argument." >&2
-      exit 1
-      ;;
+      	echo "Option -$OPTARG requires an argument." >&2
+      	exit 1
+      	;;
   esac
 done
   
